344ca1afdc2ca40efbb35079e5ebba76249a279d
Raymond McPherson <raymond.a.mcpherson@gmail.com>
2018-02-19 15:32:58 +0000

    Creation.

---
+++ triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def perimeter(side1, side2, side3):
+    '''(number, number, number) -> number
+
+    Return the perimeter of the triangle with sides of
+    length side1, side2 and side3.
+
+    >>> perimeter(3, 4, 5)
+    12
+    >>> perimeter(10.5, 6, 9.3)
+    25.8
+    '''
+
+    return side1 + side2 + side3
+
+
